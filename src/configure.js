ffb3559e9a6f8edd443a411174c53a8a256b2211
andrew stoker <andrew.stoker@thomsonreuters.com>
2014-11-11 09:44:59 +0000

    Default kata

--- src/configure.js
+++ src/configure.js
@@ -1,9 +1,17 @@
 'use strict';
 
-module.exports = function (/* overrides */) {
-    return {
+
+   var _ = require('lodash');
+
+module.exports = function (overrides) {
+    var return_object = {
         user:     'user',
         password: 'password',
         url:      'http://github.com'
     };
+
+
+    _.merge(return_object,overrides);
+
+    return return_object;
 };
